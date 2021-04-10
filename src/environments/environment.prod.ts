2cbf4bc66959227fa4e11c5b1b29dadcda4d50d3
b-blots-tran <62266562+b-blots-tran@users.noreply.github.com>
2021-04-09 21:29:28 -0400

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientID: 'cb075be3b32f4062a62b4aa389cf9297',
+  clientSecret: '41a3b24983374d79949f6d9d6727c27d',
+  userAPIBase: 'https://secure-sea-70308.herokuapp.com/api/user'
 };
